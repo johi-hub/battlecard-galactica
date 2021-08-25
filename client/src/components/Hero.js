a0edfd4bb2f332b4fff28adc7b062d43f1c5c1ed
“johanna <jd.34736.jdlt@gmail.com>
2021-08-25 16:42:20 -0400

    cleaned up code, fixed typos

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -10,18 +10,18 @@ const Section = styled.section`
 height: 100vh;
 display: flex;
 justify-content: center;
-lign-items: center;
+align-items: center;
 background: #131313
 `;
 
 const Container = styled.div`
 display: grid;
-gride-template-columns: 1fr 1fr;
+grid-template-columns: 1fr 1fr;
 height: 100vh;
-padding: 3rem calc((100vw - 13000px) /2);
+padding: 3rem calc((100vw - 1300px) /2);
 
 @media screen and (max-width: 768px) {
-    grid-grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
 }
 `;
 
@@ -62,7 +62,6 @@ width: 100%;
 height: 100%;
 max-width: 250px;
 max-height: 250px;
-
 `;
 
 const ColumnRight = styled.div`
@@ -91,13 +90,12 @@ ${Image}:nth-child(4) {
     bottom: 100px;
     right: 75px;
 }
-
 `;
 
 const Hero = () => {
     const fadeLeft = {
         hidden: { opacity: 0, x: -100 },
-        visible: {opacity: 1, x: 0}
+        visible: { opacity: 1, x: 0 }
     }
     return (
         <Section>
@@ -114,7 +112,8 @@ const Hero = () => {
                         initial='hidden'
                         animate='visible'
                         transition={{ duration: 1 }}
-                    >Journey to the unknown</motion.p>
+                        >
+                        Journey to the unknown</motion.p>
                     <Button
                         whileHover={{ scale: 1.05 }}
                         whileTap={{
@@ -125,28 +124,48 @@ const Hero = () => {
                         }}
                         initial={{ opacity: 0 }}
                         animate={{opacity: 1, transition: {duration: 1.5} }}
-                    >
+                        >
                         Get Started</Button>
                 </ColumnLeft>
                 <ColumnRight>
-                    <Image src={BattleshipOne} alt='battleship' whileTap={{ scale: 0.9 }}
-                    drag={true}
+                    <Image
+                        src={BattleshipOne}
+                        alt='battleship'
+                        whileTap={{ scale: 0.9 }}
+                        drag={true}
                         dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50 }}
                         initial={{ opacity: 0, y: -100 }}
                         animate={{opacity: 1, y: 0, transition: {duration: 1}}}
                     />
-                    <Image src={BattleshipTwo} alt='battleship'
-                    whileTap={{ scale: 0.6 }}
-                    drag={true}
+                    <Image
+                        src={BattleshipTwo}
+                        alt='battleship'
+                        whileTap={{ scale: 0.6 }}
+                        drag={true}
                         dragConstraints={{ left: 50, right: 0, top: 0, bottom: 50 }}
                         initial={{ opacity: 0, x: 100 }}
-                        animate={{opacity: 1, x: 0, transition: {duration: 1}}}
+                        animate={{opacity: 1, x: 0, transition: { duration: 1 } }}
                     />
-                    <Image src={BattleshipThree} alt='battleship' />
-                    <Image src={BattleshipFour} alt='battleship'/>
+                    <Image
+                        src={BattleshipThree}
+                        alt='battleship'
+                        whileTap={{ scale: 0.8 }}
+                        drag={true}
+                        dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50 }}
+                        initial={{ opacity: 0, x: -100 }}
+                        animate={{opacity: 1, x: 0, transition: { duration: 1 } }}
+                    />
+                    <Image
+                        src={BattleshipFour}
+                        alt='battleship'
+                        whileTap={{ scale: 0.9 }}
+                        drag={true}
+                        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+                        initial={{ opacity: 0, y: 100 }}
+                        animate={{opacity: 1, y: 0, transition: { duration: 1 } }}
+                    />
                 </ColumnRight>
             </Container>
-            
         </Section>
     )
 };
