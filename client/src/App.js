72057b25c341efb8c32cac360e9bfdf3a1f6fc47
“johanna <jd.34736.jdlt@gmail.com>
2021-08-27 16:26:58 -0400

    installed react-router-dom to root directory. imported browserrouter, switch, route from react-router-dom to App.js. Added Route components to App function. Route /welcome to Hero component. Route /homepage to Homepage component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import '../src/assets/css/App.css';
 import Hero from './components/Hero';
+import Homepage from './components/Homepage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <><Hero /> </>;
+  return (
+    <>
+      <Router>
+        <Route path="/welcome" component={ Hero }/>
+        <Route path="/homepage" component={ Homepage }/>
+      </Router>
+    </>
+  );
 }
 
 export default App;
